6e7d7641d6a3bb2f4369022637898b9f517c61d4
Anna Nilsson <annamcn98@hotmail.com>
2023-01-13 23:51:49 +0100

    knappar på plats men är fula

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import bluevase from '../bluevase.png'
-import { Link } from 'react-router-dom'
+// import component1 from '../component1.png'
+// import { Link } from 'react-router-dom'
 
 
 
@@ -11,6 +12,7 @@ function Homepage() {
     <div>
     <Wrapper>
       <LeftDiv>
+        {/* <ImgComponent src={component1} alt="abstract"></ImgComponent> */}
         <h1 className='Linkfont'>
           All you need is love, some flowers and a good vase..
         </h1>
@@ -27,6 +29,9 @@ function Homepage() {
           Shoppa blommor
           </Btn2>
         </ButtonDiv>
+        <div>
+          
+        </div>
         </div>
   )
 }
@@ -47,17 +52,22 @@ margin: 1.5em 0;
 const Btn1 = styled.button`
   height: 30px;
   margin-right: 0.5em;
+  background-color: #EEB5A2;
+  border: none;
+  
 `
 const Btn2 = styled.button`
 height: 30px;
 margin-left: 0.5em;
+background-color: #EEB5A2;
+border: none;
 
 `
 const LeftDiv = styled.div`
     display: flex;
     height: 200px;
     width: 40%;
-    background-color: aliceblue;
+    background-color: white;
 
 `
 const RightDiv = styled.div`
@@ -66,8 +76,11 @@ align-items: center;
     display: flex;
     height: 200px;
     width: 20%;
-    background-color: aliceblue;
+    background-color: white;
 `
 const Img = styled.img `
+height: 350px;
+`
+const ImgComponent = styled.img `
 height: 350px;
 `
