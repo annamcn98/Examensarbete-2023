3de65f211a514c47070388791e68fdeda9ebfc96
EVELINA <evelinaghazzaoui@hotmail.com>
2023-01-19 13:54:32 +0100

    Hämtar produkt

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,47 +1,56 @@
-import React, {useState, useEffect} from 'react'
-import { useParams } from "react-router-dom"
-// import styled from 'styled-components'
-// import Nav from '../components/Nav';
+import { useParams} from "react-router-dom"
+import { useEffect, useState } from "react";
+import woocommerceRestApi from '@woocommerce/woocommerce-rest-api';
 
+const api = new woocommerceRestApi({
+    url: 'https://anna-nilsson.cme-projects.com/',
+    consumerKey: 'ck_7e751480edcce32808fa57574c16630a267d8cce',
+    consumerSecret:'cs_768af23bdd31b6188b9a13ffabf4af6bfbb6f167',
+    wpAPIPrefix: "examen/wp-json",
+  });
+  
+  export async function fetchWooCommerceProducts() {
+      try {
+          const response = await api.get("products");
+          // console.log(response);
+          return response;
+        } catch (error) {
+          throw new Error(error);
+        }
+      }
 
 function Product() {
-  const params = useParams();
-  console.log(params)
   const [product, setProduct] = useState([]);
+  const { id } = useParams(); // get the id from the URL
 
   const fetchProduct = async () => {
-  try {
-    const response = await fetch('https://codexplained.se/electronics.php?id=' + params.id);
-    const data = await response.json();
-    console.log(data)
-    
-    setProduct(data);
-  } catch (error) {
-    console.log (error)
+    try {
+      const response = await fetchWooCommerceProducts().catch((error)=>(error));
+      const data = response.data;
+      setProduct(data.filter(p => p.id == id)[0]); // filter the list of products to find the one with the matching id
+    } catch (error) {
+      console.log (error)
+    }
   }
 
-}
   useEffect(() => {
     fetchProduct();
   },)
 
   return (
-
     <div>
       <div>
-            <div>
-              <img src={product.images && product.images[0].src} alt=""></img>
-            </div>
-            <article>
-              <h2>{product.name}</h2>
-              <p>p: {product.description}</p>
-              <h3>Storage: {product.stock} left in stock</h3>
-              <h2>{product.price} SEK</h2>
-            </article>
-      </div>
-
-    </div>
-
+        <div>
+          <img src={product.images && product.images[0].src} alt=""></img>
+        </div>
+        <article>
+          <h2>{product.name}</h2>
+          <p> {product.description}</p>
+          <h2>{product.price} KR</h2>
+        </article>
+      </div> 
+    
+    </div> 
   )
 }
 export default Product;
